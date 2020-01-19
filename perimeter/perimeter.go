6c874722793c9ada5b8d22095209e173c8b23823
Sho Hoshino <sho.hoshino@hacobu.jp>
2020-01-19 17:51:44 +0900

    refactor with using interface

--- perimeter/perimeter.go
+++ perimeter/perimeter.go
@@ -1,10 +1,34 @@
 package perimeter
 
+import "math"
 
-func Perimeter(width, height float64) float64 {
-	return 2 * (width + height)
+type Rectangle struct {
+	Width float64
+	Height float64
 }
 
-func Area(width, height float64) float64 {
-	return width * height
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
 }
+
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+type Triangle struct {
+	Base float64
+	Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return t.Base * t.Height / 2
+}
+
+func Perimeter(rectangle Rectangle ) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
+}
+
--- perimeter/perimeter_test.go
+++ perimeter/perimeter_test.go
@@ -2,8 +2,13 @@ package perimeter
 
 import "testing"
 
+type Shape interface {
+	Area() float64
+}
+
 func TestPerimeter(t *testing.T) {
-	got := Perimeter(10.0, 10.0)
+	rectangle := Rectangle{10.0, 10.0}
+	got := Perimeter(rectangle)
 	want := 40.0
 
 	if got != want {
@@ -13,10 +18,34 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	got := Area(12.0, 6.0)
-	want := 72.0
-
-	if got != want {
-		t.Errorf("got %.2f, want %.2f", got, want)
+	areaTests := []struct {
+		name  string
+		shape Shape
+		want  float64
+	}{
+		{
+			"Rectangle",
+			Rectangle{Width: 12, Height: 6,},
+			72.0,
+		},
+		{
+			"Circle",
+			Circle{Radius: 10},
+			314.1592653589793,
+		},
+		{
+			"Triangle",
+			Triangle{Base: 12, Height: 6},
+			36.0,
+		},
 	}
+
+	for _, tc := range areaTests {
+		got := tc.shape.Area()
+		want := tc.want
+		if got != want {
+			t.Errorf("%#v got %g want %g", tc.name, got, tc.want)
+		}
+	}
+
 }
