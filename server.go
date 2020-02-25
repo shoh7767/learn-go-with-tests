cb95288cc5179a15a475f5e142315bba7142ac38
Sho Hoshino <shoh0918@gmail.com>
2020-02-25 18:37:51 +0900

    implement server by TDD

---
+++ server.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+type PlayerStore interface {
+	GetPlayerScore(name string) int
+	RecordWin(name string)
+}
+
+type PlayerServer struct {
+	Store PlayerStore
+}
+
+func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	player := strings.TrimPrefix(r.URL.Path, "/players/")
+
+	switch r.Method {
+	case http.MethodPost:
+		p.processWin(w, r, player)
+	case http.MethodGet:
+		p.showScore(w, r, player)
+	}
+}
+
+func (p *PlayerServer) showScore(w http.ResponseWriter, r *http.Request, player string) {
+	score := p.Store.GetPlayerScore(player)
+	if score == 0 {
+		w.WriteHeader(http.StatusNotFound)
+	}
+	fmt.Fprint(w, score)
+}
+
+func (p *PlayerServer) processWin(w http.ResponseWriter, r *http.Request, player string) {
+	p.Store.RecordWin(player)
+	w.WriteHeader(http.StatusAccepted)
+}
